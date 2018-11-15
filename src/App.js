30a6e0fc66b325e42ad3b530e47d3dc37aba4886
VLAD <bejiec0607@gmail.com>
2018-11-15 16:00:45 +0200

    sacvsd

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter, Route} from 'react-router-dom'
+import {AddressBookNew} from './AddressBookNewEdit'
+import {AddressBookEdit} from './AddressBookNewEdit'
+import AddressBook from "./AddressBook";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component{
+
+    render (){
+        return (
+            <BrowserRouter>
+                <div>
+                    <Route path="/" component={AddressBook}/>
+                    <Route exact path="/New" component={AddressBookNew}/>
+                    <Route exact path="/Edit" component={AddressBookEdit}/>
+                </div>
+            </BrowserRouter>
+        )
+    }
 }
 
 export default App;
+
