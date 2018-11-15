bb5fe1e14da6c97def7a6f681a2e3f610089d31c
VLAD <bejiec0607@gmail.com>
2018-11-15 16:21:04 +0200

    sacvsd

---
+++ src/Firestore.js
@@ -0,0 +1,11 @@
+import {firebase} from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyAtRzAx-NHymA2yLuu5287nbNb-FfF4UOE",
+    authDomain: "reconapp-c629a.firebaseapp.com",
+    databaseURL: "https://reconapp-c629a.firebaseio.com",
+    projectId: "reconapp-c629a",
+    storageBucket: "reconapp-c629a.appspot.com",
+    messagingSenderId: "298910673843"
+};
+firebase.initializeApp(config);
