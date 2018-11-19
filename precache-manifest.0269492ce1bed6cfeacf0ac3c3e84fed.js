04656328c6024e4b24ea9e60a8c58a4c8477522b
VLAD <bejiec0607@gmail.com>
2018-11-19 10:33:09 +0200

    Updates

---
+++ precache-manifest.0269492ce1bed6cfeacf0ac3c3e84fed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2d47e3b1f30e3b45d820cde6775a3b1",
+    "url": "/ReconTask/static/media/plus.c2d47e3b.svg"
+  },
+  {
+    "revision": "41125cd8a7f7ab9e91c74a4e522ebbec",
+    "url": "/ReconTask/static/media/cancel.41125cd8.svg"
+  },
+  {
+    "revision": "8982d314091795f9ab4f",
+    "url": "/ReconTask/static/js/runtime~main.8982d314.js"
+  },
+  {
+    "revision": "1f2fa9e53888cc6efac7",
+    "url": "/ReconTask/static/js/main.1f2fa9e5.chunk.js"
+  },
+  {
+    "revision": "799e3f10106173f794d9",
+    "url": "/ReconTask/static/js/1.799e3f10.chunk.js"
+  },
+  {
+    "revision": "1f2fa9e53888cc6efac7",
+    "url": "/ReconTask/static/css/main.7cb91e08.chunk.css"
+  },
+  {
+    "revision": "f7dafaa1e5cef990b48c0bbc6b4ce423",
+    "url": "/ReconTask/index.html"
+  }
+];
